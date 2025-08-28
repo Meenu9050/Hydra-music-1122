2f2eb6a5b46aa630395a5a884c65967c788ac6f9
Meenu9050 <mmsharma9050@gmail.com>
2025-08-28 20:37:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID", None))
-OWNER_USERNAME = os.getenv("OWNER_USERNAME", "@GodxSharma")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "@HydraxMusic_bot")
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "GodxSharma")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "HydraxMusic_bot")
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # 🛠 Database & Deployment Configs
